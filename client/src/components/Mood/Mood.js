93eb7cf762a1afe7cbfadf70ac14317b90ed8620
Fysics7 <j.mills.ucf.cbc@gmail.com>
2018-10-24 18:42:45 -0400

    updated image

--- client/src/components/Mood/Mood.js
+++ client/src/components/Mood/Mood.js
@@ -4,7 +4,7 @@ class AudioPlayer extends React.Component {
     render() {
       return (
         <div>
-          <audio ref="audio_tag" src="../assets/Sci-Fi-RPG-Intro.mp3" controls autoPlay/>
+          <audio ref="audio_tag" src="client/src/assets/Sci-Fi-RPG-Intro.mp3" controls autoPlay/>
           <p className="loginMusic">Music by Eric Matyas - www.soundimage.org</p>
         </div>
       );
